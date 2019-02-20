037cfbe7bb5f96725bf6a4f1edc3da2b681a2204
Leo <leo.pan.duh@gmail.com>
2019-02-20 17:55:20 -0500

    add text for draw. change way grid is made

--- src/index.js
+++ src/index.js
@@ -24,25 +24,16 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    let grid = [];
+    for (let i = 0; i < 3; i++ ) {
+      let rows = [];
+      for( let j = 0; j < 3; j++) {
+        rows.push(this.renderSquare(i*3 + j));
+      }
+      grid.push(<div className="board-row">{rows}</div>);
+    }
+
+    return (<div>{grid}</div>);
   }
 }
 
@@ -101,6 +92,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (this.state.stepNumber >= 9) {
+      status = 'Draw'
     } else {
       status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
     }
